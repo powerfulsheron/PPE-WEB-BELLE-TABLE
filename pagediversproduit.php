9cc6a6e4171aed0da019c517bca4728ed30bc6f3
AxelleS <saillyaxelle@hotmail.fr>
2017-02-28 11:07:18 +0100

    Update pagediversproduit.php

--- pagediversproduit.php
+++ pagediversproduit.php
@@ -144,12 +144,6 @@ $result = $bdd->query('SELECT * FROM `t_gamme` WHERE `numcateg` LIKE '.$typeprod
     </div>
     <!-- /.container -->
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
 </body>
 
 </html>
