0d3c087b6f3fc2efe589164276bb81570a22c7b0
AxelleS <saillyaxelle@hotmail.fr>
2017-02-28 15:45:11 +0100

    Update pageinspi.php

--- pageinspi.php
+++ pageinspi.php
@@ -129,6 +129,7 @@ if(isset($_SESSION['login'])){
             <div class="row">
                 <div class="encadrefooter">
                     <p>Copyright &copy; BelleTable 2017</p>
+					<p><a href="mentionlegale.php">Mentions Légales</a></p>
                 </div>
             </div>
         </footer>
