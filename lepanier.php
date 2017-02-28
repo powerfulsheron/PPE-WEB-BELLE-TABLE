291958e7c28480607b78fb21755698ee3ba675dc
AxelleS <saillyaxelle@hotmail.fr>
2017-02-28 15:44:30 +0100

    Update lepanier.php

--- lepanier.php
+++ lepanier.php
@@ -29,7 +29,9 @@ $n = 0;
 
 <head>
 
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="SIOSLAM2017">
 
@@ -165,6 +167,7 @@ $n = 0;
 				<div class="row">
 					<div class="encadrefooter">
 						<p>Copyright &copy; BelleTable 2017</p>
+						<p><a href="mentionlegale.php">Mentions Légales</a></p>
 					</div>
 				</div>
 			</footer>
