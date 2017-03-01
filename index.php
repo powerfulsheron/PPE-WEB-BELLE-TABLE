2160eaf95a859ac248ede104b7f256172f5cce57
AxelleS <saillyaxelle@hotmail.fr>
2017-03-01 10:47:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -79,6 +79,12 @@ if(isset($_SESSION['login'])){
 		<footer>
 			<div class="row">
 				<div class="encadrefooter">
+					<ul class="footer">
+					<li><a href="mentionlegale.php">Mentions Légales</a>
+					<li>&nbsp;
+					<li><a href="doc/CGV.pdf" target="_blank">Conditions générales de vente</a>
+					</ul>
+					<br/>
 					<p>Copyright &copy; BelleTable 2017</p>
 				</div>
 			</div>
