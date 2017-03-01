7fcd0d74667baa457852b60dd460637d8e78a3c9
AxelleS <saillyaxelle@hotmail.fr>
2017-03-01 10:49:54 +0100

    Update pagediversproduit.php

--- pagediversproduit.php
+++ pagediversproduit.php
@@ -138,9 +138,14 @@ $result = $bdd->query('SELECT * FROM `t_gamme` WHERE `numcateg` LIKE '.$typeprod
         <footer>
             <div class="row">
                 <div class="encadrefooter">
-                    <p>Copyright &copy; BelleTable 2017</p>
-					<p><a href="mentionlegale.php">Mentions Légales</a></p>
-                </div>
+			<ul class="footer">
+			<li><a href="mentionlegale.php">Mentions Légales</a>
+			<li>&nbsp;
+			<li><a href="doc/CGV.pdf" target="_blank">Conditions générales de vente</a>
+			</ul>
+			<br/>
+			<p>Copyright &copy; BelleTable 2017</p>
+		</div>
             </div>
         </footer>
 
