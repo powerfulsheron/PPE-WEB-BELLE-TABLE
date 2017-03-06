203c8e4e5dbbf40228b7d237369cf736fa84ccf8
AxelleS <saillyaxelle@hotmail.fr>
2017-03-06 10:53:25 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -2,21 +2,71 @@
 session_start();
 include('fonction.php');
 if(isset($_SESSION['login'])){
-	if($_SESSION['login'] != ""){
-		$menuchange = true;
-	}	
+	$menuchange = true;
 }
-if(isset($_REQUEST['email'])){
-	$nom=$_REQUEST['nom'];
-    $email=$_REQUEST['email'];
-    $message=$_REQUEST['message'];
+if(isset($_POST['email'])){
+	$nom=$_POST['nom'];
+    $email=$_POST['email'];
+	$numero=$_POST['numero'];
+    $message=$_POST['message'];
     if (($nom=="")||($email=="")||($message=="")){
-        Contact($email,$message,$nom);
+        $verif = 0;
 	}
+	$verif = Contact($email,$numero,$message,$nom);
+	echo $verif;
 }
 	
 ?>
-<?php include('header.php'); ?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>BelleTable - Elegance a la Francaise</title>
+
+    <link href="css/cssbelletable.css" rel="stylesheet">
+
+    <link href="css/shop-homepage.css" rel="stylesheet">
+
+</head>
+
+<body>
+	 <br/>
+	<div id="menuprincipal" align="center">
+		<ul class="barremenu">
+			<li>
+				<a href="index.php"><img src="img/logo.png" alt="" width="150px"></a>	
+			<li>
+				<a href="pageproduits.php">Nos Produits</a>
+			<li>
+				<a href="pageinspi.php">Nos Inspirations</a>
+			<li>
+				<a href="apropos.php">A Propos</a>
+			<li>
+				<a href="contact.php">Contact</a>
+			<li>
+				<?php
+				if(isset($menuchange)){
+					echo'
+					<a href="commandeencours.php">Mon Compte</a>
+                    <li>
+					<a href="lepanier.php">Mon Panier</a>';
+				}
+				else{
+					echo'
+					<a href="connexion.php">Connexion</a>';
+				}
+
+				?>
+		</ul>
+	</div>
+	<br/>
 	
 	<div class="contenupage">
 		<div class="container">
@@ -24,11 +74,13 @@ if(isset($_REQUEST['email'])){
 				<div class="formulairenevoi">
 					<h1>Contactez-Nous</h1>
 					<p>Pour nous contacter veuillez remplir ce formulaire.</p><br/>
-					<form  action="contact.php" id="myform" method="GET" enctype="multipart/form-data">
+					<form  action="contact.php" id="myform" method="POST" enctype="multipart/form-data">
 						<p>Votre Nom et Prénom :</p>
 						<input name="nom" type="text" value="" size="30"/><br><br>
 						<p>Votre Email :</p>
 						<input name="email" type="text" value="" size="30"/><br><br>
+						<p>Votre Numéro de téléphone :</p>
+						<input name="numero" type="text" value="" size="30"/><br><br>
 						<p>Votre Message :</p>
 						<textarea name="message" rows="7" cols="35"></textarea><br><br>
 						<input type="submit" id="envoimail" value="Envoyer" onclick="document.forms[\'form\'].submit();"/>
@@ -40,7 +92,21 @@ if(isset($_REQUEST['email'])){
 	</div>
 	
 	
-	<?php include('footer.php'); ?>
+	<div class="divfooter">
+        <hr>
+        <footer>
+        	<div class="socialnet">
+        	<a href="http://twitter.com/share" target="_blank" class="twitter-share-button" data-count="vertical" data-via="Belle_TableSIO"><img src="twitter.png" height="5%" width="5%"></a>
+		<a name="fb_share" type="box_count" href="https://www.facebook.com/Belle-Table-1113642382077898/" target="_blank"><img src ="fb.jpg" height="6%" width="6%"></a>
+		<script src="http://static.ak.fbcdn.net/connect.php/js/FB.Share" type="text/javascript"></script>
+		</div>
+			<ul class="footer">
+			<li class="lifooter"><a href="mentionlegale.php">Mentions Légales</a></li>
+			<li class="lifooter">Copyright &copy; BelleTable 2017</li>
+			<li class="lifooter"><a href="doc/CGV.pdf" target="_blank">Conditions générales de vente</a></li>
+			</ul>
+        </footer>
+    </div>
 	
 </body>
 </html>
